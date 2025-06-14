5e0c3b883df9bbbe71109fef7f5c418a613a7f08
continous-learner <3417555490@qq.com>
2025-06-14 16:44:14 +0800

    variables

--- UDPClient.java
+++ UDPClient.java
@@ -1,10 +1,19 @@
 import java.io.IOException;
+import java.util.List;
 
 public class UDPClient {
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             System.out.println("Usage: java UDPClient <host> <port> <filelist.txt>");
             return;
+        }
+        
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        List<String> files = readFileList(args[2]);
+
+        for (String filename : files) {
+            downloadFile(host, port, filename);
         }
     }
 }
