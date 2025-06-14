1aa75ca061303cc09fd06f0a353698408694b153
continous-learner <3417555490@qq.com>
2025-06-14 16:23:24 +0800

    variables

--- UDPServer.java
+++ UDPServer.java
@@ -1,3 +1,11 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class UDPServer {
+    private static final int DATA_PORT_START = 50000;
+    private static final int DATA_PORT_END = 51000;
+    private static final ExecutorService executor = Executors.newCachedThreadPool();
+    private static int nextDataPort = DATA_PORT_START;
+
     
 }
