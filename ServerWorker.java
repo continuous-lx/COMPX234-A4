e410e848b427cd876145c3ce3332426820d94d36
continous-learner <3417555490@qq.com>
2025-06-14 16:35:59 +0800

    constructor

--- ServerWorker.java
+++ ServerWorker.java
@@ -5,4 +5,11 @@ public class ServerWorker implements Runnable {
     private int fileSize;
     private InetAddress clientAddress;
     private int port;
+
+    public ServerWorker(String filename, int fileSize, InetAddress clientAddress, int port) {
+        this.filename = filename;
+        this.fileSize = fileSize;
+        this.clientAddress = clientAddress;
+        this.port = port;
+    }
 }
