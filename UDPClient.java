6fe13e14c0ec79f2b13a0aa6d4970c09c9b0f7db
continous-learner <3417555490@qq.com>
2025-06-14 16:43:15 +0800

    error

--- UDPClient.java
+++ UDPClient.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 
 public class UDPClient {
     public static void main(String[] args) throws IOException {
-        
+        if (args.length != 3) {
+            System.out.println("Usage: java UDPClient <host> <port> <filelist.txt>");
+            return;
+        }
     }
 }
